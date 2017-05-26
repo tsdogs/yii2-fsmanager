f9fa245d3dd95a7631a0841c14d0ab1c47ef077d
Alessandro Briosi <ab1@metalit.com>
2017-05-26 18:10:29 +0200

    translation and remove unused code

---
+++ src/messages/it/fsmanager.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Cancel' => 'Annulla',
+    'Create directory' => 'Crea cartella',
+    'Upload' => 'Carica files',
+    'Delete' => 'Elimina',
+    'Documents' => 'Documenti',
+];
